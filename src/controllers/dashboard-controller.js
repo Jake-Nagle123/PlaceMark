4dcad62bdfe70fd4aa61a344bdf760d687ab13e3
Jake Nagle <jake.nagle54@gmail.com>
2025-03-12 19:00:51 +0000

    update

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,7 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      return h.view("main");
+      const events = await db.eventStore.getAllEvents();
+      const viewData = {
+        title: "Event Dashboard",
+        events: events,
+      };
+      return h.view("dashboard-view", viewData);
+    },
+  },
+
+  addEvent: {
+    handler: async function (request, h) {
+      const newEvent = {
+        title: request.payload.title,
+      };
+      await db.eventStore.addEvent(newEvent);
+      return h.redirect("/dashboard");
     },
   },
 };
