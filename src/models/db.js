4b37e3831be88271687563b4fc85a09eaad228d7
Jake Nagle <jake.nagle54@gmail.com>
2025-03-17 14:28:33 +0000

    Updated file

--- src/models/db.js
+++ src/models/db.js
@@ -1,12 +1,15 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { eventMemStore } from "./mem/event-mem-store.js";
+import { stadiumMemStore } from "./mem/stadium-mem-store.js";
 
 export const db = {
   userStore: null,
   eventStore: null,
+  stadiumStore: null,
 
   init() {
     this.userStore = userMemStore;
     this.eventStore = eventMemStore;
+    this.stadiumStore = stadiumMemStore;
   },
 };
