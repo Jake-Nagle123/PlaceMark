a7236dee9adc534bde3de0de7a8d790941a2ce47
Jake Nagle <jake.nagle54@gmail.com>
2025-03-17 15:52:24 +0000

    Updated file

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -2,16 +2,19 @@ import { db } from "../models/db.js";
 
 export const accountsController = {
   index: {
+    auth: false,
     handler: function (request, h) {
       return h.view("main", { title: "Welcome to Event" });
     },
   },
   showSignup: {
+    auth: false,
     handler: function (request, h) {
       return h.view("signup-view", { title: "Sign up for Event" });
     },
   },
   signup: {
+    auth: false,
     handler: async function (request, h) {
       const user = request.payload;
       await db.userStore.addUser(user);
@@ -19,22 +22,25 @@ export const accountsController = {
     },
   },
   showLogin: {
+    auth: false,
     handler: function (request, h) {
       return h.view("login-view", { title: "Login to Event" });
     },
   },
   login: {
+    auth: false,
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
       if (!user || user.password !== password) {
         return h.redirect("/");
       }
-      request.cookieAuth.set({ id: user._id });
+      request.cookieAuth.set({ id: user._id })
       return h.redirect("/dashboard");
     },
   },
   logout: {
+    auth: false,
     handler: function (request, h) {
       return h.redirect("/");
     },
