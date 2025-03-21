18472e01adff8dce4e5b10acd0929bf4b0550c90
Jake Nagle <jake.nagle54@gmail.com>
2025-03-21 16:08:02 +0000

    Updated file

---
+++ src/models/mongo/event-mongo-store.js
@@ -0,0 +1,44 @@
+import Mongoose from "mongoose";
+import { Event } from "./event.js";
+import { stadiumMongoStore } from "./stadium-mongo-store.js";
+
+export const eventMongoStore = {
+  async getAllEvents() {
+    const events = await Event.find().lean();
+    return events;
+  },
+
+  async getEventById(id) {
+    if (Mongoose.isValidObjectId(id)) {
+      const event = await Event.findOne({ _id: id }).lean();
+      if (event) {
+        event.stadiums = await stadiumMongoStore.getStadiumsByEventId(event._id);
+      }
+      return event;
+    }
+    return null;
+  },
+
+  async addEvent(event) {
+    const newEvent = new Event(event);
+    const eventObj = await newEvent.save();
+    return this.getEventById(eventObj._id);
+  },
+
+  async getUserEvents(id) {
+    const event = await Event.find({ userid: id }).lean();
+    return event;
+  },
+
+  async deleteEventById(id) {
+    try {
+      await Event.deleteOne({ _id: id });
+    } catch (error) {
+      console.log("bad id");
+    }
+  },
+
+  async deleteAllEvents() {
+    await Event.deleteMany({});
+  }
+};
