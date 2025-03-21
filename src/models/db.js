d1145ef9a29a48d2baec6012795cf73b18cff365
Jake Nagle <jake.nagle54@gmail.com>
2025-03-21 12:39:52 +0000

    Updated file

--- src/models/db.js
+++ src/models/db.js
@@ -4,6 +4,8 @@ import { stadiumMemStore } from "./mem/stadium-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
 import { eventJsonStore } from "./json/event-json-store.js";
 import { stadiumJsonStore } from "./json/stadium-json-store.js";
+import { connectMongo } from "./mongo/connect.js";
+import { userMongoStore } from "./mongo/user-mongo-store.js";
 
 export const db = {
   userStore: null,
@@ -17,6 +19,10 @@ export const db = {
         this.eventStore = eventJsonStore;
         this.stadiumStore = stadiumJsonStore;
         break;
+      case "mongo":
+        this.userStore = userMongoStore;
+        connectMongo();
+        break;      
       default:
         this.userStore = userMemStore;
         this.eventStore = eventMemStore;
