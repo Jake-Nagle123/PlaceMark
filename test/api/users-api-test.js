cea0edc388879f2ae8c9ea01ebbad1fe711de14f
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 15:01:46 +0000

    Updated file

--- test/api/users-api-test.js
+++ test/api/users-api-test.js
@@ -32,4 +32,25 @@ suite("User API tests", () => {
     const returnedUser = await eventService.getUser(testUsers[0]._id);
     assert.deepEqual(testUsers[0], returnedUser);
   });
+
+  test("get a user - bad id", async () => {
+    try {
+      const returnedUser = await eventService.getUser("1234");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 503);
+    }
+  });
+
+  test("get a user - deleted user", async () => {
+    await eventService.deleteAllUsers();
+    try {
+      const returnedUser = await eventService.getUser(testUsers[0]._id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 404);
+    }
+  });
 });
