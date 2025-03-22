54dc1f409702b86221642a314ae8753757beb1da
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 14:44:24 +0000

    Content added to file -  test api methods

--- test/api/users-api-test.js
+++ test/api/users-api-test.js
@@ -1,11 +1,35 @@
+import { assert } from "chai";
 import { eventService } from "./event-service.js";
+import { assertSubset } from "../test-utils.js";
+import { kevin, testUsers } from "../fixtures.js";
 
 suite("User API tests", () => {
   setup(async () => {
+    await eventService.deleteAllUsers();
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testUsers[i] = await eventService.createUser(testUsers[i]);
+    }
   });
   teardown(async () => {
   });
 
   test("create a user", async () => {
+    const newUser = await eventService.createUser(kevin);
+    assertSubset(kevin, newUser);
+    assert.isDefined(newUser._id);
+  });
+
+  test("delete all users", async () => {
+    let returnedUsers = await eventService.getAllUsers();
+    assert.equal(returnedUsers.length, 3);
+    await eventService.deleteAllUsers();
+    returnedUsers = await eventService.getAllUsers();
+    assert.equal(returnedUsers.length, 0);
+  });
+
+  test("get a user - success", async () => {
+    const returnedUser = await eventService.getUser(testUsers[0]._id);
+    assert.deepEqual(testUsers[0], returnedUser);
   });
 });
