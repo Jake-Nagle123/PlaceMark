1c9e321d60cc80c2c306bd7810b6ed5e769f228d
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 11:32:17 +0000

    Moved to new folder

--- test/models/stadium-model-tests.js
+++ test/models/stadium-model-tests.js
@@ -1,7 +1,7 @@
 import { assert } from "chai";
-import { db } from "../src/models/db.js";
-import { testEvents, testStadiums, game, trip, gameday, testUsers } from "./fixtures.js";
-import { assertSubset } from "./test-utils.js";
+import { db } from "../../src/models/db.js";
+import { testEvents, testStadiums, game, trip, gameday, testUsers } from "../fixtures.js";
+import { assertSubset } from "../test-utils.js";
 
 suite("Stadium Model tests", () => {
 
