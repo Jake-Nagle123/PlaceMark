398111e6ff485def8cc9e7733078035b6e399435
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 15:14:12 +0000

    Content added to file

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,8 +1,15 @@
 import { userApi } from "./api/user-api.js";
+import { eventApi } from "./api/event-api.js";
 
 export const apiRoutes = [
   { method: "GET", path: "/api/users", config: userApi.find },
   { method: "POST", path: "/api/users", config: userApi.create },
   { method: "DELETE", path: "/api/users", config: userApi.deleteAll },
   { method: "GET", path: "/api/users/{id}", config: userApi.findOne },
+
+  { method: "POST", path: "/api/events", config: eventApi.create },
+  { method: "DELETE", path: "/api/events", config: eventApi.deleteAll },
+  { method: "GET", path: "/api/events", config: eventApi.find },
+  { method: "GET", path: "/api/events/{id}", config: eventApi.findOne },
+  { method: "DELETE", path: "/api/events/{id}", config: eventApi.deleteOne },
 ];
