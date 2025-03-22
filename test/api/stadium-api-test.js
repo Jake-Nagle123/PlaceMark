eedce68ba1c1b7b4d81b33db84470df950f28252
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 17:21:46 +0000

    Content added to file - api test methods (basic)

---
+++ test/api/stadium-api-test.js
@@ -0,0 +1,26 @@
+import { assert } from "chai";
+import { assertSubset } from "../test-utils.js";
+import { eventService } from "./event-service.js";
+import { kevin, trip, testEvents, testStadiums, gameday } from "../fixtures.js";
+
+suite("Stadium API tests", () => {
+  let user = null;
+  let weekOne = null;
+
+  setup(async () => {
+  });
+
+  teardown(async () => {});
+
+  test("create stadium", async () => {
+  });
+
+  test("create Multiple stadiums", async () => {
+  });
+
+  test("Delete stadium", async () => {
+  });
+
+  test("test denormalised stadium", async () => {
+  });
+});
