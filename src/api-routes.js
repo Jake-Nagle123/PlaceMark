bec56c4dce356b568c44474eab2be61f0d22217c
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 17:10:07 +0000

    Content added to file - api routes

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,5 +1,6 @@
 import { userApi } from "./api/user-api.js";
 import { eventApi } from "./api/event-api.js";
+import { stadiumApi } from "./api/stadium-api.js";
 
 export const apiRoutes = [
   { method: "GET", path: "/api/users", config: userApi.find },
@@ -12,4 +13,10 @@ export const apiRoutes = [
   { method: "GET", path: "/api/events", config: eventApi.find },
   { method: "GET", path: "/api/events/{id}", config: eventApi.findOne },
   { method: "DELETE", path: "/api/events/{id}", config: eventApi.deleteOne },
+
+  { method: "GET", path: "/api/stadiums", config: stadiumApi.find },
+  { method: "GET", path: "/api/stadiums/{id}", config: stadiumApi.findOne },
+  { method: "POST", path: "/api/playlists/{id}/stadiums", config: stadiumApi.create },
+  { method: "DELETE", path: "/api/stadiums", config: stadiumApi.deleteAll },
+  { method: "DELETE", path: "/api/stadiums/{id}", config: stadiumApi.deleteOne },
 ];
