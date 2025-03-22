8bef8614e1f6b1b84acaaaff48e52531a20e5bcb
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 16:26:07 +0000

    Updated file

--- src/api/event-api.js
+++ src/api/event-api.js
@@ -11,18 +11,47 @@ export const eventApi = {
   findOne: {
     auth: false,
     async handler(request) {
+      try {
+        const event = await db.eventStore.getEventById(request.params.id);
+        if (!event) {
+          return Boom.notFound("No Event with this id");
+        }
+        return event;
+      } catch (err) {
+        return Boom.serverUnavailable("No Event with this id");
+      }
     },
   },
 
   create: {
     auth: false,
     handler: async function (request, h) {
+      try {
+        const event = request.payload;
+        const newEvent = await db.eventStore.addEvent(event);
+        if (newEvent) {
+          return h.response(newEvent).code(201);
+        }
+        return Boom.badImplementation("error creating event");
+      } catch (err) {
+        return Boom.serverUnavailable("Database Error");
+      }
     },
   },
 
   deleteOne: {
     auth: false,
     handler: async function (request, h) {
+      try {
+        const event = await db.eventStore.getEventById(request.params.id);
+        if (!event) {
+          return Boom.notFound("No Event with this id");
+        }
+        await db.eventStore.deleteEventById(event._id);
+        return h.response().code(204);
+      } catch (err) {
+        return Boom.serverUnavailable("No Event with this id");
+      }
     },
   },
 
