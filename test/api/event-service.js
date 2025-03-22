a06b33c733f92cfe25555b28719865e1072ed746
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 14:36:19 +0000

    Updated file

--- test/api/event-service.js
+++ test/api/event-service.js
@@ -1,3 +1,12 @@
-export const eventService = {
+import axios from "axios";
 
-};
+import { serviceUrl } from "../fixtures.js";
+
+export const eventService = {
+  eventUrl: serviceUrl,
+
+  async createUser(user) {
+    const res = await axios.post(`${this.eventUrl}/api/users`, user);
+    return res.data;
+  }
+}
