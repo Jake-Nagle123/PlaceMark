a91d1a6ad0456127f7a7f48cbdddf1a8147bb88b
Jake Nagle <jake.nagle54@gmail.com>
2025-03-22 17:56:56 +0000

    Content added to file -  test api methods

--- test/api/stadium-api-test.js
+++ test/api/stadium-api-test.js
@@ -8,19 +8,59 @@ suite("Stadium API tests", () => {
   let weekOne = null;
 
   setup(async () => {
+    await eventService.deleteAllEvents();
+    await eventService.deleteAllUsers();
+    await eventService.deleteAllStadiums();
+    user = await eventService.createUser(kevin);
+    trip.userid = user._id;
+    weekOne = await eventService.createEvent(trip);
   });
 
   teardown(async () => {});
 
   test("create stadium", async () => {
+    const returnedStadium = await eventService.createStadium(weekOne._id, gameday);
+    assertSubset(gameday, returnedStadium);
   });
 
   test("create Multiple stadiums", async () => {
+    for (let i = 0; i < testStadiums.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await eventService.createStadium(weekOne._id, testStadiums[i]);
+    }
+    const returnedStadiums = await eventService.getAllStadiums();
+    assert.equal(returnedStadiums.length, testStadiums.length);
+    for (let i = 0; i < returnedStadiums.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const stadium = await eventService.getStadium(returnedStadiums[i]._id);
+      assertSubset(stadium, returnedStadiums[i]);
+    }
   });
 
-  test("Delete stadium", async () => {
+  test("Delete StadiumApi", async () => {
+    for (let i = 0; i < testStadiums.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await eventService.createStadium(weekOne._id, testStadiums[i]);
+    }
+    let returnedStadiums = await eventService.getAllStadiums();
+    assert.equal(returnedStadiums.length, testStadiums.length);
+    for (let i = 0; i < returnedStadiums.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const stadium = await eventService.deleteStadium(returnedStadiums[i]._id);
+    }
+    returnedStadiums = await eventService.getAllStadiums();
+    assert.equal(returnedStadiums.length, 0);
   });
 
-  test("test denormalised stadium", async () => {
+  test("denormalised stadium", async () => {
+    for (let i = 0; i < testStadiums.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await eventService.createStadium(weekOne._id, testStadiums[i]);
+    }
+    const returnedStadium = await eventService.getEvent(weekOne._id);
+    assert.equal(returnedStadium.stadiums.length, testStadiums.length);
+    for (let i = 0; i < testStadiums.length; i += 1) {
+      assertSubset(testStadiums[i], returnedStadium.stadiums[i]);
+    }
   });
 });
