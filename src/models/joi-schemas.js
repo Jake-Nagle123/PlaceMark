f2584d6042902c85683fc8d87be9a7841eb42c9f
Jake Nagle <jake.nagle54@gmail.com>
2025-03-23 12:36:59 +0000

    Updated file

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,16 +1,20 @@
 import Joi from "joi";
 
-export const UserSpec = {
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-};
-
 export const UserCredentialsSpec = {
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 };
+
+export const UserSpec = Joi.object()
+  .keys({
+    firstName: Joi.string().example("Kevin").required(),
+    lastName: Joi.string().example("Malone").required(),
+    email: Joi.string().email().example("kevin@malone.com").required(),
+    password: Joi.string().example("office").required(),
+  })
+  .label("UserDetails");
+
+export const UserArray = Joi.array().items(UserSpec).label("UserArray");
 
 export const StadiumSpec = {
   stadium: Joi.string().required(),
