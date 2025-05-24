ba8f9d897d73a09882a7f3a07ea039ad2b08399c
Jake Nagle <jake.nagle54@gmail.com>
2025-05-24 13:15:42 +0100

    Updated the joi-schema with new details on the user

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -2,23 +2,24 @@ import Joi from "joi";
 
 export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
 
-export const UserCredentialsSpec = {
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-};
+export const UserCredentialsSpec = Joi.object()
+.keys({
+  email: Joi.string().email().example("kevin@malone.com").required(),
+  password: Joi.string().example("office").required(),
+})
+.label("UserCredentials");
 
-export const UserSpec = Joi.object()
-  .keys({
+export const UserSpec = UserCredentialsSpec.keys({
     firstName: Joi.string().example("Kevin").required(),
     lastName: Joi.string().example("Malone").required(),
-    email: Joi.string().email().example("kevin@malone.com").required(),
-    password: Joi.string().example("office").required(),
-    _id: IdSpec,
-    __v: Joi.number(),
-  })
-  .label("UserDetails");
+  }).label("UserDetails");
 
-export const UserArray = Joi.array().items(UserSpec).label("UserArray");
+export const UserSpecPlus = UserSpec.keys({
+  _id: IdSpec,
+  __v: Joi.number(),
+}).label("UserDetailsPlus")
+
+export const UserArray = Joi.array().items(UserSpecPlus).label("UserArray");
 
 export const StadiumSpec = {
   stadium: Joi.string().required(),
