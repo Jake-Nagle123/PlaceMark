c4e76b53579d7f4204a4533e1acd1474e8730ec9
Jake Nagle <jake.nagle54@gmail.com>
2025-05-25 11:22:25 +0100

    Updated the users api test and all tests pass

--- test/api/users-api-test.js
+++ test/api/users-api-test.js
@@ -8,11 +8,16 @@ const users = new Array(testUsers.length);
 
 suite("User API tests", () => {
   setup(async () => {
+    eventService.clearAuth();
+    await eventService.createUser(kevin);
+    await eventService.authenticate(kevin);
     await eventService.deleteAllUsers();
     for (let i = 0; i < testUsers.length; i += 1) {
       // eslint-disable-next-line no-await-in-loop
       users[0] = await eventService.createUser(testUsers[i]);
     }
+    await eventService.createUser(kevin);
+    await eventService.authenticate(kevin);
   });
   teardown(async () => {});
 
@@ -22,12 +27,14 @@ suite("User API tests", () => {
     assert.isDefined(newUser._id);
   });
 
-  test("delete all userApi", async () => {
+  test("delete all users", async () => {
     let returnedUsers = await eventService.getAllUsers();
-    assert.equal(returnedUsers.length, 3);
+    assert.equal(returnedUsers.length, 4);
     await eventService.deleteAllUsers();
+    await eventService.createUser(kevin);
+    await eventService.authenticate(kevin);
     returnedUsers = await eventService.getAllUsers();
-    assert.equal(returnedUsers.length, 0);
+    assert.equal(returnedUsers.length, 1);  
   });
 
   test("get a user - success", async () => {
@@ -41,12 +48,14 @@ suite("User API tests", () => {
       assert.fail("Should not return a response");
     } catch (error) {
       assert(error.response.data.message === "No User with this id");
-//    assert.equal(error.response.data.statusCode, 503);
+      assert.equal(error.response.data.statusCode, 404);
     }
   });
 
   test("get a user - deleted user", async () => {
     await eventService.deleteAllUsers();
+    await eventService.createUser(kevin);
+    await eventService.authenticate(kevin);
     try {
       const returnedUser = await eventService.getUser(users[0]._id);
       assert.fail("Should not return a response");
