5cabd9b2df032b5061efb5614572bd423a24a543
Jake Nagle <jake.nagle54@gmail.com>
2025-05-25 12:56:27 +0100

    Created new schema for Jwt Auth

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -54,3 +54,10 @@ export const EventSpecPlus = EventSpec.keys({
 }).label("EventPlus");
 
 export const EventArraySpec = Joi.array().items(EventSpecPlus).label("EventArray");
+
+export const JwtAuth = Joi.object()
+  .keys({
+    success: Joi.boolean().example("true").required(),
+    token: Joi.string().example("eyJhbGciOiJND.g5YmJisIjoiaGYwNTNjAOhE.gCWGmY5-YigQw0DCBo").required(),
+})
+.label("JwtAuth");
