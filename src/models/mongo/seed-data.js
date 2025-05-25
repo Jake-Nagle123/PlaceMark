d068fc82bd1041caea0477a0d979ba103d01f217
Jake Nagle <jake.nagle54@gmail.com>
2025-05-25 19:42:39 +0100

    Added new seed data file with user details data

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,23 @@
+export const seedData = {
+  users: {
+    _model: "User",
+    kevin: {
+      firstName: "Kevin",
+      lastName: "Malone",
+      email: "kevin@malone.com",
+      password: "office"
+    },
+    pam: {
+      firstName: "Pam",
+      lastName: "Beesly",
+      email: "pam@beesly.com",
+      password: "office"
+    },
+    jim: {
+      firstName: "Jim",
+      lastName: "Halpert",
+      email: "jim@halpert.com",
+      password: "office"
+    }
+  },
+};
