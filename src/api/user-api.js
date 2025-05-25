1d413f6d1817dfab2acab20d9c4ae8a3078448e2
Jake Nagle <jake.nagle54@gmail.com>
2025-05-25 14:39:17 +0100

    Updated user api with a validatation imported from the schema

--- src/api/user-api.js
+++ src/api/user-api.js
@@ -1,7 +1,7 @@
 import Boom from "@hapi/boom";
 import { createToken } from "./jwt-utils.js";
 import { db } from "../models/db.js";
-import { UserSpec, UserSpecPlus, IdSpec, UserArray, JwtAuth } from "../models/joi-schemas.js";
+import { UserCredentialsSpec, UserSpec, UserSpecPlus, IdSpec, UserArray, JwtAuth } from "../models/joi-schemas.js";
 import { validationError } from "./logger.js";
 
 export const userApi = {
@@ -102,6 +102,7 @@ export const userApi = {
     tags: ["api"],
     description: "Authenticate a user",
     notes: "If user has valid email/password, create and return a JWT token",
+    validate: { payload: UserCredentialsSpec, failAction: validationError },
     response: { schema: JwtAuth, failAction: validationError },
   },
 };
