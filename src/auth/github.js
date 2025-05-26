e324b98caf21cb439db98b9f97a3d7167c044f6e
Jake Nagle <jake.nagle54@gmail.com>
2025-05-26 17:10:00 +0100

    Added session to github Auth module

--- src/auth/github.js
+++ src/auth/github.js
@@ -1,20 +1,36 @@
 import Bell from "bell";
+import Cookie from "hapi-auth-cookie";
 
 async function register (server, options) {  
-  await server.register({
-    plugin: Bell
-});
+    await server.register({
+      plugin: Bell
+    },
+    {
+      plugin: Cookie
+    }
+  );
+
+  /**
+   * Register 'github' authentication strategy
+   */
+  server.auth.strategy("github", "bell", {
+    provider: "github",
+    password: "ThisIsASecretCookiePasswordForGitHub",
+    clientId: "Ov23liXTZO0xKofGBPf9",
+    clientSecret: "e9f5c3557549419014b4a63e823c509c0bb58723",
+    isSecure: process.env.NODE_ENV === "production"
+  })
 
 /**
- * Register 'github' authentication strategy
+ * Register session based auth strategy to store
+ * credentials received from GitHub and keep
+ * the user logged in
  */
-server.auth.strategy("github", "bell", {
-  provider: "github",
-  password: "ThisIsASecretCookiePasswordForGitHub",
-  clientId: "Ov23liXTZO0xKofGBPf9",
-  clientSecret: "e9f5c3557549419014b4a63e823c509c0bb58723",
-  isSecure: process.env.NODE_ENV === "production"
-})
+  server.auth.strategy("session", "cookie", {
+    password: "ThisIsASecretPasswordForTheAuthCookie",
+    redirectTo: "/",
+    isSecure: process.env.NODE_ENV === "production"
+  })
 
 server.log("info", "Plugin registered: bell authentication with strategy »github«")
 }
