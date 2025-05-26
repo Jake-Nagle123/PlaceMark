06223206c8d610455acff315482b65fb4cae4038
Jake Nagle <jake.nagle54@gmail.com>
2025-05-26 16:12:46 +0100

    Added auth folder and github folder with content for github OAuth

---
+++ src/auth/github.js
@@ -0,0 +1,27 @@
+import Bell from "bell";
+
+async function register (server, options) {  
+  await server.register({
+    plugin: Bell
+});
+
+/**
+ * Register 'github' authentication strategy
+ */
+server.auth.strategy("github", "bell", {
+  provider: "github",
+  password: "ThisIsASecretCookiePasswordForGitHub",
+  clientId: "Ov23liXTZO0xKofGBPf9",
+  clientSecret: "e9f5c3557549419014b4a63e823c509c0bb58723",
+  isSecure: process.env.NODE_ENV === "production"
+})
+
+server.log("info", "Plugin registered: bell authentication with strategy »github«")
+}
+
+export const plugin = {  
+  register,
+  name: "authentication",
+  version: "1.0.0",
+  once: true
+}
