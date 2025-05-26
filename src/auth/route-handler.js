663c68b6fbfd9ed1a9c6b9e511bc3ef12870e5d9
Jake Nagle <jake.nagle54@gmail.com>
2025-05-26 19:06:06 +0100

    Adding files got Github Oauth

---
+++ src/auth/route-handler.js
@@ -0,0 +1,26 @@
+
+
+server.route({  
+  method: "GET",
+  path: "/auth/github",
+  options: {
+    auth: "github",
+    handler: (request, h) => {
+      if (request.auth.isAuthenticated) {
+        const user = request.auth.credentials.profile
+        const data = {
+          name: user.displayName,
+          username: user.username,
+          avatar: user.raw.avatar_url
+        }
+
+        request.cookieAuth.set(data);
+
+        return h.view("authenticated", data);
+      }
+      return h.view("index", {
+        error: "Could not authenticate with GitHub."
+      }).code(400);
+    }
+  }
+});
--- src/server.js
+++ src/server.js
@@ -85,29 +85,7 @@ async function init() {
   verifyOptions: { algorithms: ["HS256"] },
   });
   server.auth.default("session");
-
-  server.route({  
-    method: "GET",
-    path: "/auth/github",
-    options: {
-      auth: "github",
-      handler: (request, h) => {
-        if (request.auth.isAuthenticated) {
-          const user = request.auth.credentials.profile
-          const data = {
-            name: user.displayName,
-            username: user.username,
-            avatar: user.raw.avatar_url
-          }
-          return h.view("authenticated", data)
-        }
-        return h.view("index", {
-          error: "Could not authenticate with GitHub."
-        }).code(400)
-      }
-    }
-  });
-
+  
   db.init("mongo");
   server.route(webRoutes);
   server.route(apiRoutes);
