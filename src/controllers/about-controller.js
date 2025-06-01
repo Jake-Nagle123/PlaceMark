dc60bb03b337c9975f0bb877b27e4691402f8b1f
Jake Nagle <jake.nagle54@gmail.com>
2025-06-01 17:47:18 +0100

    Added login auth request details to about view

--- src/controllers/about-controller.js
+++ src/controllers/about-controller.js
@@ -1,8 +1,13 @@
 export const aboutController = {
   index: {
     handler: function (request, h) {
+      const loggedInUser = request.auth.credentials;
+      if (!loggedInUser._id) {
+        return h.view("main", { title: "View About page error" }).code(404);
+      }
       const viewData = {
         title: "About Placemark",
+        userid: loggedInUser._id,
       };
       return h.view("about-view", viewData);
     },
